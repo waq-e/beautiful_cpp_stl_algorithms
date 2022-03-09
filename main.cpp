8c7105af3dfa1a87fd013cbf44bc4e8a30e2e1b3
waq-e <whomomlikes@hotmail.com>
2022-03-08 22:45:18 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Headers you should know:
+//      <algorithm>
+//      <vector> <array> <list> <stack> <map> <queue>
+//      <string>
+//      ..maybe <iterator>
+//      ..maybe <utility> <tuple>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
