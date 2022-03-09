9de8b8a114af6b6ebd9a95743e74a9bef549b36f
waq-e <whomomlikes@hotmail.com>
2022-03-08 23:24:01 -0600

    Using loop vs <algorithm> to count item in a container.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 // Headers you should know:
 //      <algorithm>
@@ -8,6 +9,22 @@
 //      ..maybe <utility> <tuple>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	// Section 1 Section 1 Section 1 Section 1 Section 1 Section 1 Section 1 Section 1 Section 1
+	// =========================================================================================
+	// count how many entries in collection have the target value (2)
+	std::vector<int> v{1, 2, 5, 3, 2, 4, 5, 1, 3, 2};
+	int twos{0};
+	int const target{2};
+	for (size_t i{0}; i < v.size(); i++) {
+		if (v[i] == target) twos++;
+	}
+	std::cout << "value of twos: " << twos << '\n';
+	// using <algorithm> to do above
+	int simple = std::count(v.begin(), v.end(), target);
+	std::cout << "value of simple: " << simple << '\n';
+
+
+
 	return 0;
 }
